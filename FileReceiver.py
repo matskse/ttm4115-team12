ea72e3b69e67ff39cf8a9413221abd2c0022a6d7
matskse <mkse1995@gmail.com>
2021-04-24 18:14:35 +0200

    Components send error message when losing connection

--- FileReceiver.py
+++ FileReceiver.py
@@ -10,21 +10,36 @@ class FileReceiverComponent:
         self.driver.send('message', 'stm')
     
     def on_connect(self, client, userdata, flags, rc):
-        print('MQTT connected to {}'.format(client))
+        print('File receiver connected')
+        self.connected = True
+    
+    def on_disconnect(self, client, userdata, rc):
+        print('File receiver disconnected')
+        self.connected = False
+        self.driver.send('error_message', 'stm')
     
     def __init__(self, driver, MQTT_BROKER, MQTT_PORT):
-        
+        self.MQTT_BROKER = MQTT_BROKER
+        self.MQTT_PORT = MQTT_PORT
         self.mqtt_client = mqtt.Client()
         
         self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_disconnect = self.on_disconnect
         self.mqtt_client.on_message = self.on_message
-        self.mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
+        self.mqtt_client.connect(self.MQTT_BROKER, self.MQTT_PORT)
         self.mqtt_client.loop_start()
 
         self.driver = driver
+
+        self.connected = False
     
     def subscribe_to_topic(self, topic):
         self.mqtt_client.subscribe(topic, 0)
     
     def unsubscribe_from_topic(self, topic):
         self.mqtt_client.unsubscribe(topic, 0)
+    
+
+    def disconnect(self):
+        self.mqtt_client.disconnect()
+            
--- FileSender.py
+++ FileSender.py
@@ -6,17 +6,27 @@ class FileSenderComponent:
         print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
     def on_connect(self, client, userdata, flags, rc):
-        print('MQTT connected to {}'.format(client))
+        print('File sender connected')
+        self.connected = True
+    
+    def on_disconnect(self, client, userdata, rc):
+        print('File sender disconnected')
+        self.connected = False
+        self.driver.send('error_message', 'stm')
     
     def __init__(self, driver, MQTT_BROKER, MQTT_PORT):
+        self.MQTT_BROKER = MQTT_BROKER
+        self.MQTT_PORT = MQTT_PORT
         self.mqtt_client = mqtt.Client()
         
         self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_disconnect = self.on_disconnect
         self.mqtt_client.on_message = self.on_message
-        self.mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
+        self.mqtt_client.connect(self.MQTT_BROKER, self.MQTT_PORT)
         self.mqtt_client.loop_start()
 
         self.driver = driver
+        self.connected = False
 
         
     
@@ -30,4 +40,8 @@ class FileSenderComponent:
         self.mqtt_client.publish(topic, payload=byteArray, qos=0)
 
         self.driver.send('message_sent', 'stm')
+    
+    def disconnect(self):
+        self.mqtt_client.loop_stop()
+        self.mqtt_client.disconnect()
         
